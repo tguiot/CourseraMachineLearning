bb4bd66d680c17457c8fb759ce93d6d9230210d9
Thomas Guiot <thoma@T-1GuiotPort.bordet.be>
2019-12-25 11:10:49 +0100

    ex7 after solving

--- machine-learning-ex7/ex7/computeCentroids.m
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -26,7 +26,11 @@ centroids = zeros(K, n);
 % Note: You can use a for-loop over the centroids to compute this.
 %
 
-
+for j=1:K
+  p=zeros(m,n);
+  p(idx==j,:)=1;
+  centroids(j,:)=1/sum(idx==j)*sum(X.*p);
+endfor
 
 
 
--- machine-learning-ex7/ex7/findClosestCentroids.m
+++ machine-learning-ex7/ex7/findClosestCentroids.m
@@ -20,11 +20,13 @@ idx = zeros(size(X,1), 1);
 %
 % Note: You can use a for-loop over the examples to compute this.
 %
-
-
-
-
-
+m=size(X,1);
+distance = zeros(m,K);
+for j=1:K
+  D = bsxfun(@minus, X, centroids(j,:));
+  distance(:,j)=sum(D.^2,2);
+endfor
+[minval,idx]=min(distance,[],2);
 
 
 % =============================================================
