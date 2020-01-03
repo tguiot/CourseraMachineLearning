79eedce081042c891c977b23060423ec501e8955
tguiot <guiothomas@gmail.com>
2020-01-03 10:50:38 +0100

    ex8 solved

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -40,20 +40,12 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+err = X*Theta'-Y;
 
+J = 1/2*(sum(sum((err.*R).^2)) + lambda*(sum(sum(Theta.^2)) + sum(sum(X.^2))));
 
-
-
-
-
-
-
-
-
-
-
-
-
+X_grad = err.*R*Theta + lambda*X;
+Theta_grad = (err.*R)'*X + lambda*Theta;
 
 % =============================================================
 
